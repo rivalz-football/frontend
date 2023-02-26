7de8a544c9e766dd9c1443d594b5b5086a8b4fff
tolgaand <tolgaand@yandex.com>
2023-02-27 01:54:53 +0300

    fix: eslint issues

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
+export default function MyDocument() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
