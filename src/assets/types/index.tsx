89829327d417d23e7ed61051ab4684cda6731110
tolgaand <tolgaand@yandex.com>
2023-02-27 19:37:31 +0300

    chore: fix responsive problems

--- src/assets/types/index.tsx
+++ src/assets/types/index.tsx
@@ -1,24 +1,15 @@
 import { StaticImageData } from "next/image";
 
-export type CardItemType = {
-    image: StaticImageData;
-    name: string;
-    isAvailable: boolean;
-    amount: number;
-  };
-export type CardPropsType = {
-    card: {
-      image: StaticImageData;
-      name: string;
-      isAvailable: boolean;
-      amount: number;
-    };
-  };
-  
-export  enum CardType {
-    BEGINNER = "BEGINNER",
-    SEMI_PRO = "SEMI-PRO",
-    PROFESSIONAL = "PROFESSIONAL",
-    LEGENDARY = "LEGENDARY",
-  }
+export enum CardType {
+  BEGINNER = "BEGINNER",
+  SEMI_PRO = "SEMI-PRO",
+  PROFESSIONAL = "PROFESSIONAL",
+  LEGENDARY = "LEGENDARY",
+}
 
+export type IChooseCard = {
+  image: StaticImageData;
+  name: string;
+  isAvailable: boolean;
+  count: number;
+};
