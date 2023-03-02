16da4465a1a2747163b06f7b7d33073a993b2d95
unknown <ecaepraw@gmail.com>
2023-03-03 01:12:06 +0300

    add sidebar burger animation

--- src/components/Dashboard/Header.tsx
+++ src/components/Dashboard/Header.tsx
@@ -14,21 +14,40 @@ import {
 import { DownArrowIcon, GiftIcon, NotificationIcon } from "assets/icons";
 import { SolanaImage } from "assets/images";
 import { useSidebar } from "contexts/SidebarContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Notification } from "./Notification";
 import { UserAvatar } from "./UserAvatar";
 import { UserBalance } from "./UserBalance";
 import { CgArrowsExchange } from "react-icons/cg";
-
+import { CSSTransition } from "react-transition-group";
 export const Header = () => {
   const { toggleSidebar } = useSidebar();
+  const [isShaking, setIsShaking] = useState(false);
 
+  const handleClick = () => {
+    toggleSidebar();
+    setIsShaking(true);
+    setTimeout(() => {
+      setIsShaking(false);
+    }, 1000);
+  };
   return (
     <>
-      <CgArrowsExchange
-        onClick={toggleSidebar}
-        style={{ width: "30px", height: "30px" }}
-      />
+      <CSSTransition in={isShaking} timeout={1000} classNames="shake">
+        <CgArrowsExchange
+          onClick={handleClick}
+          style={{
+            width: "35px",
+            height: "35px",
+            cursor: "pointer",
+            background: "rgb(70, 65, 65)",
+            borderRadius: "5px",
+            padding: "5px",
+            opacity: 0.8,
+          }}
+        />
+      </CSSTransition>
+
       <Flex alignItems="center" gap="15px">
         <UserBalance image={SolanaImage.src} name="$SOL" balance={123} />
         <Notification count={4} countColor="#EC068D" icon={NotificationIcon} />
