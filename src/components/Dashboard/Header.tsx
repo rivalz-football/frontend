1072151ea61ec842b8cb6ef0ed57d577cee63905
tolgaand <tolgaand@yandex.com>
2023-03-13 23:31:54 +0300

    chore: undo Header

--- src/components/Dashboard/Header.tsx
+++ src/components/Dashboard/Header.tsx
@@ -1,40 +1,20 @@
-import {
-  Avatar,
-  Box,
-  Button,
-  DrawerCloseButton,
-  Flex,
-  HStack,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-  StatDownArrow,
-  useDisclosure,
-  useToken,
-} from "@chakra-ui/react";
+import { Flex, useDisclosure, useToken } from "@chakra-ui/react";
 import { DownArrowIcon, GiftIcon, NotificationIcon } from "assets/icons";
 import { SolanaImage } from "assets/images";
 import { useSidebar } from "contexts/SidebarContext";
-import { useEffect, useState } from "react";
 import { Notification } from "./Notification";
 import { UserAvatar } from "./UserAvatar";
 import { UserBalance } from "./UserBalance";
 import { CgArrowsExchange } from "react-icons/cg";
-import { CSSTransition } from "react-transition-group";
 
-import { TransactionPopup } from "components/common/TransactionPopup";
-import { useTokens } from "hooks/useToken";
-import { IToken, IUserToken } from "assets/types";
 import { useMeSolanaBalance, useMeTokens } from "hooks/useUser";
 import { SOL_MINT_ADDRESS } from "assets/data";
 
 export const Header = () => {
   const { toggleSidebar } = useSidebar();
   const { isOpen, onClose, onOpen } = useDisclosure();
-  const { data: token, isLoading } = useMeTokens(SOL_MINT_ADDRESS);
-  const { data: balance, isLoading: balanceLoading } = useMeSolanaBalance();
+  // const { data: token, isLoading } = useMeTokens(SOL_MINT_ADDRESS);
+  const { data: balance, isLoading } = useMeSolanaBalance();
   return (
     <>
       <CgArrowsExchange
@@ -51,7 +31,7 @@ export const Header = () => {
       />
 
       <Flex alignItems="center" gap="15px">
-        <Button
+        {/* <Button
           background="rgba(217, 217, 217, 0.01)"
           border="1px solid rgba(255, 255, 255, 0.02)"
           borderRadius="4px"
@@ -61,19 +41,22 @@ export const Header = () => {
           onClick={onOpen}
         >
           Withdraw
-        </Button>
-        {!isLoading && token && !balanceLoading && balance && (
+        </Button> */}
+        {/* {!isLoading && token && !balanceLoading && balance && ( */}
+        {!isLoading && (
           <UserBalance
-            image={token.information.image}
-            name={token.information.symbol}
-            balance={balance}
+            image="https://i.imgur.com/Q6fjbgI.png"
+            name="$SOL"
+            balance={balance || 0}
           />
         )}
+
+        {/* )} */}
         <Notification count={4} countColor="#EC068D" icon={NotificationIcon} />
         <Notification count={4} countColor="#D95543" icon={GiftIcon} />
         <UserAvatar avatar="" />
       </Flex>
-      <TransactionPopup isOpen={isOpen} onClose={onClose} token={token} />
+      {/* <TransactionPopup isOpen={isOpen} onClose={onClose} token={token} /> */}
     </>
   );
 };
